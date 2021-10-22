452071dd4ee9cc663ff89d46fedf953203a73c5b
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2021-10-22 16:31:04 -0300

    Cria a classe AtividadeDAO

---
+++ src/br/com/healthTrack/entities/dao/AtividadeDAO.java
@@ -0,0 +1,21 @@
+package br.com.healthTrack.entities.dao;
+
+import java.sql.Date;
+
+import br.com.healthTrack.interfaces.InterfaceAtividade;
+
+public class AtividadeDAO implements InterfaceAtividade{
+
+	@Override
+	public void getAll() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void insert(Long idUsuario, Long idExercicio, Date dataAtividade) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
