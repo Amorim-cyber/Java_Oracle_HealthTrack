2729fa019f5748d263710152e84b2057caed295a
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2021-10-22 15:12:40 -0300

    Adiciona classe Pressao

---
+++ src/br/com/fiap/entities/Pressao.java
@@ -0,0 +1,49 @@
+package br.com.fiap.entities;
+
+import java.sql.Date;
+
+public class Pressao {
+	
+	private Long idPressao;
+	private Double valorPressao;
+	private Date registroPressao;
+	private Long idUsuario;
+	
+	public Long getIdPressao() {
+		return idPressao;
+	}
+	public void setIdPressao(Long idPressao) {
+		this.idPressao = idPressao;
+	}
+	public Double getValorPressao() {
+		return valorPressao;
+	}
+	public void setValorPressao(Double valorPressao) {
+		this.valorPressao = valorPressao;
+	}
+	public Date getRegistroPressao() {
+		return registroPressao;
+	}
+	public void setRegistroPressao(Date registroPressao) {
+		this.registroPressao = registroPressao;
+	}
+	public Long getIdUsuario() {
+		return idUsuario;
+	}
+	public void setIdUsuario(Long idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+	@Override
+	public String toString() {
+		return "Pressao [idPressao=" + idPressao + 
+				", valorPressao=" + valorPressao + 
+				", registroPressao=" + registroPressao + 
+				", idUsuario=" + idUsuario + 
+				"]";
+	}
+	
+	
+	
+	
+
+}
