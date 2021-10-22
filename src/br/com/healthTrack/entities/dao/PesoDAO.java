2fb8212f6520fb8c0f40ebbde5607f28a6e0d82f
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2021-10-22 16:36:12 -0300

    Cria a classe PesoDAO

---
+++ src/br/com/healthTrack/entities/dao/PesoDAO.java
@@ -0,0 +1,21 @@
+package br.com.healthTrack.entities.dao;
+
+import java.sql.Date;
+
+import br.com.healthTrack.interfaces.InterfacePeso;
+
+public class PesoDAO implements InterfacePeso{
+
+	@Override
+	public void getAll() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void insert(Double valorPeso, Date registroPeso, Long idUsuario) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
