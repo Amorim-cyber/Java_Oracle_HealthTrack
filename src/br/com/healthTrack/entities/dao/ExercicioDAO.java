1c4ecfc49e83519c6f5e57d8448ea00b39fd9871
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2021-10-22 16:32:55 -0300

    Cria a classe ExercicioDAO

---
+++ src/br/com/healthTrack/entities/dao/ExercicioDAO.java
@@ -0,0 +1,19 @@
+package br.com.healthTrack.entities.dao;
+
+import br.com.healthTrack.interfaces.InterfaceExercicio;
+
+public class ExercicioDAO implements InterfaceExercicio{
+
+	@Override
+	public void getAll() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void insert(String nomeExercicio, Integer setsExercicio, Integer repeticaoExercicio) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
