2b013c2273a5f5c3af906c6c6643845ddfb19752
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2021-10-22 16:27:21 -0300

    Cria a classe alimentoDAO

---
+++ src/br/com/healthTrack/entities/dao/AlimentoDAO.java
@@ -0,0 +1,21 @@
+package br.com.healthTrack.entities.dao;
+
+import java.sql.Date;
+
+import br.com.healthTrack.interfaces.InterfaceAlimento;
+
+public class AlimentoDAO implements InterfaceAlimento{
+
+	@Override
+	public void getAll() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void insert(String nomeAlimento, Double caloriasAlimento, Date horaConsumo, Long idUsuario) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
