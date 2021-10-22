6af04f87a262ab533824ae8eb36c277327b90073
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2021-10-22 16:37:34 -0300

    Cria a classe PressaoDAO

---
+++ src/br/com/healthTrack/entities/dao/PressaoDAO.java
@@ -0,0 +1,21 @@
+package br.com.healthTrack.entities.dao;
+
+import java.sql.Date;
+
+import br.com.healthTrack.interfaces.InterfacePressao;
+
+public class PressaoDAO implements InterfacePressao{
+
+	@Override
+	public void getAll() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void insert(Double valorPressao, Date registroPressao, Long idUsuario) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
