07874a249fbadb8db9828395ba7c586c1a9565af
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2021-10-22 18:40:35 -0300

    Popula classe Teste com registro e listagem de Atividade

--- src/br/com/healthTrack/main/Teste.java
+++ src/br/com/healthTrack/main/Teste.java
@@ -1,17 +1,26 @@
 package br.com.healthTrack.main;
 
-import java.sql.Connection;
+import java.time.LocalDate;
 
-import br.com.healthTrack.dbConnection.DBManager;
+import br.com.healthTrack.entities.dao.AtividadeDAO;
 
 public class Teste {
 
 	public static void main(String[] args) {
 		
-		Connection connection = DBManager.obterConexao();
+		LocalDate todayLocalDate = LocalDate.now( );
 		
+		System.out.println("--------------------REGISTRO DE ATIVIDADE---------------------");
 		
-		System.out.println(connection);
+		AtividadeDAO atividadeDAO = new  AtividadeDAO();
+		
+		atividadeDAO.insert(2l, 1l, java.sql.Date.valueOf( todayLocalDate ));
+		atividadeDAO.insert(2l, 2l, java.sql.Date.valueOf( todayLocalDate ));
+		atividadeDAO.insert(2l, 3l, java.sql.Date.valueOf( todayLocalDate ));
+		atividadeDAO.insert(1l, 1l, java.sql.Date.valueOf( todayLocalDate ));
+		atividadeDAO.insert(1l, 3l, java.sql.Date.valueOf( todayLocalDate ));
+		
+		atividadeDAO.getAll();
 
 	}
 
