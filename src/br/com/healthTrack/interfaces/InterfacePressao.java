572e3b1ce95a17bef7d2f9f9ef108697676357c5
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2021-10-25 11:47:12 -0300

    Modifica interface InterfacePressao

--- src/br/com/healthTrack/interfaces/InterfacePressao.java
+++ src/br/com/healthTrack/interfaces/InterfacePressao.java
@@ -1,11 +1,11 @@
 package br.com.healthTrack.interfaces;
 
-import java.sql.Date;
+import java.util.Calendar;
 
 public interface InterfacePressao {
 	
 	public abstract void getAll();
 	
-	public abstract void insert(Double valorPressao,Date registroPressao,Long idUsuario);
+	public abstract void insert(Double valorPressao,Calendar registroPressao,Long idUsuario);
 
 }
