6e3dea5944d1402f4d4a3eeadba54367d89a9f93
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2021-10-25 10:32:27 -0300

    Modifica classe Teste

--- src/br/com/healthTrack/main/Teste.java
+++ src/br/com/healthTrack/main/Teste.java
@@ -1,6 +1,7 @@
 package br.com.healthTrack.main;
 
 import java.util.Calendar;
+import java.util.Scanner;
 
 import br.com.healthTrack.entities.dao.AtividadeDAO;
 
@@ -8,17 +9,55 @@ public class Teste {
 
 	public static void main(String[] args) {
 		
-		System.out.println("--------------------REGISTRO DE ATIVIDADE---------------------");
+		Scanner sc = new Scanner(System.in);
+		boolean loop = true;
 		
-		AtividadeDAO atividadeDAO = new  AtividadeDAO();
+		do {
+			System.out.println("O que deseja fazer?");
+			System.out.println();
+			System.out.println();
+			System.out.println("INSTRUÇÕES:");
+			System.out.println();
+			System.out.println("Digite 1 para fazer 5 registros de atividade");
+			System.out.println("Digite 2 para visualizar registros de atividade");
+			System.out.println("Digite 3 para sair");
+			System.out.println();
+			System.out.print(">>> ");
+			
+			
+			switch(sc.next()) {
+			
+				case "1":{
+					AtividadeDAO atividadeDAO = new  AtividadeDAO();
+					atividadeDAO.insert(2l, 1l, Calendar.getInstance());
+					atividadeDAO.insert(2l, 2l, Calendar.getInstance());
+					atividadeDAO.insert(2l, 3l, Calendar.getInstance());
+					atividadeDAO.insert(1l, 1l, Calendar.getInstance());
+					atividadeDAO.insert(1l, 3l, Calendar.getInstance());
+					break;
+				}
+				
+				case "2":{
+					AtividadeDAO atividadeDAO = new  AtividadeDAO();
+					System.out.println("--------------------REGISTRO DE ATIVIDADE---------------------");
+					atividadeDAO.getAll();
+					System.out.println();
+					break;
+				}
+				
+				case "3":{
+					loop = false;
+				}
+				default:{
+					System.out.println("ERRO - COMANDO INCORRETO !!");
+				}
+			
+			}
+		}while(loop);
 		
-		atividadeDAO.insert(2l, 1l, Calendar.getInstance());
-		atividadeDAO.insert(2l, 2l, Calendar.getInstance());
-		atividadeDAO.insert(2l, 3l, Calendar.getInstance());
-		atividadeDAO.insert(1l, 1l, Calendar.getInstance());
-		atividadeDAO.insert(1l, 3l, Calendar.getInstance());
+		sc.close();
 		
-		atividadeDAO.getAll();
+		System.out.println("PROGRAMA FINALIZADO !!");
 
 	}
 
